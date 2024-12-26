a104ee7e4371774450f5c817e3dbbafabc53bf0f
vilalclara <34722004+villaclara@users.noreply.github.com>
2024-12-26 02:11:00 +0200

    /c styling main timer and selecting activities

--- babyest.client/src/app/dashboard/main-timer/main-timer.component.ts
+++ babyest.client/src/app/dashboard/main-timer/main-timer.component.ts
@@ -14,67 +14,88 @@ import { Kid } from '../../models/kid';
   styleUrl: './main-timer.component.css'
 })
 
-export class MainTimerComponent implements OnInit, OnChanges{
+export class MainTimerComponent implements OnInit, OnChanges {
 
-  constructor(private kidService : KidService) { }
-  
-  
-  isRunningTimer : boolean = false;
+  constructor(private kidService: KidService) { }
+
+
+  isRunningTimer: boolean = false;
   startStopImageLink: string = this.isRunningTimer ? '../../../assets/img/stop_icon.png' : '../../../assets/img/play_icon.png';
 
-  timerDone$ : Subject<boolean> = new Subject<boolean>();
-  timePassed : number = 0;    
+  timerDone$: Subject<boolean> = new Subject<boolean>();
+  timePassed: number = 0;
   timePass$ = timer(1, 1000).pipe(map(n => (this.timePassed + n) * 1000));
   // timePass$ = timer(1, 1000).subscribe(() => { this.timePassed += 1000; console.log(this.timePassed) });
   //a = timer(1, 1000).subscribe(() => this.timePassed +=1);
 
-  currentActivityName : string = '';
+  currentActivityName: string = '';
 
-  isEatingSelected : boolean = false;
+  isEatingSelected: boolean = false;
 
 
-  @Output() newKidActivity : EventEmitter<KidActivity> = new EventEmitter<KidActivity>();
-  
-  @Input() currentActivity : KidActivity = { ActivityType : '', Id : 0, KidName : '', StartDate : undefined, EndDate : undefined, IsActiveNow : false };
- 
+  @Output() newKidActivity: EventEmitter<KidActivity> = new EventEmitter<KidActivity>();
+
+  @Input() currentActivity: KidActivity = { ActivityType: '', Id: 0, KidName: '', StartDate: undefined, EndDate: undefined, IsActiveNow: false };
+
 
   // When the parent has set CurrentActivity property (in Http get) we want to display actual values of timer etc.
   ngOnChanges(changes: SimpleChanges): void {
     console.log(`onchanges - ${this.currentActivity.Id}`);
     console.log(this.currentActivity.EndDate);
-    if(this.currentActivity.IsActiveNow == true)
-      {
-        let timeDiff = new Date().getTime() - this.currentActivity.StartDate!.getTime();
-        this.timePassed = Math.floor(timeDiff / 1000);
-        this.isRunningTimer = true;
-        timer(1, 1000).pipe(takeUntil(this.timerDone$)).subscribe(() => this.timePassed +=1);
-        this.startStopImageLink = '../../../assets/img/stop_icon.png';  
-        this.currentActivityName = this.currentActivity.ActivityType;
+    if (this.currentActivity.IsActiveNow == true) {
+      let timeDiff = new Date().getTime() - this.currentActivity.StartDate!.getTime();
+      this.timePassed = Math.floor(timeDiff / 1000);
+      this.isRunningTimer = true;
+      timer(1, 1000).pipe(takeUntil(this.timerDone$)).subscribe(() => this.timePassed += 1);
+      this.startStopImageLink = '../../../assets/img/stop_icon.png';
+      this.currentActivityName = this.currentActivity.ActivityType;
 
-      }
+      // if ANY eating we set the IsEatingSelected to True
+      this.isEatingSelected = this.currentActivity.ActivityType.toLowerCase() != 'sleeping'
+        && this.currentActivity.ActivityType != ''
+        ? true
+        : false;
+    }
   }
 
   ngOnInit(): void {
-    
+
   }
 
-  startSelectEatingType() : void {
+  startSelectEatingType(): void {
     this.isEatingSelected = !this.isEatingSelected;
-  }
-  
-  selectActivity(actType : string) : void {
-    console.log(actType);
-    this.currentActivity.ActivityType = actType;
+
+    // Because we want to remove highlight from Sleeping (if it was selected before) as soon as we press 'Eating'.
+    // Also we disable the 'Play' button if only pressed 'Eating' without specifying exact type.
+    this.currentActivity.ActivityType = '';
   }
 
-  startActivity() : void {
+  selectActivity(actType: string): void {
+
+    // Second click on Sleeping removing the highlight.
+    if (this.currentActivity.ActivityType.toLowerCase() == 'sleeping' && actType.toLowerCase() == 'sleeping') {
+      this.currentActivity.ActivityType = '';
+    }
+    // Normal behavior
+    else {
+      this.currentActivity.ActivityType = actType;
+    }
+
+    // Highlight proper activity type.
+    this.isEatingSelected = this.currentActivity.ActivityType.toLowerCase() != 'sleeping'
+      && this.currentActivity.ActivityType != ''
+      ? true
+      : false;
+
+  }
+
+  startActivity(): void {
 
     // When timer is stopped.
     // When we want to start activity and send the currentActivity to api.
-    if(!this.isRunningTimer)
-    {
+    if (!this.isRunningTimer) {
       // Start the timer
-      interval(1000).pipe(takeUntil(this.timerDone$)).subscribe(() => this.timePassed +=1);
+      interval(1000).pipe(takeUntil(this.timerDone$)).subscribe(() => this.timePassed += 1);
       this.timePassed = 0;
       this.startStopImageLink = '../../../assets/img/stop_icon.png';
       this.isRunningTimer = true;
@@ -94,13 +115,12 @@ export class MainTimerComponent implements OnInit, OnChanges{
 
     // When timer is running.
     // When we want to stop activity and send the FULL activity to api.
-    else 
-    {
+    else {
       // Stop the timer
       this.timerDone$.next(true);
       this.startStopImageLink = '../../../assets/img/play_icon.png';
       this.isRunningTimer = false;
-
+      this.isEatingSelected = false;
 
       // Stop tracking current Activity. Set EndDate
       this.currentActivity.EndDate = new Date();
@@ -123,6 +143,6 @@ export class MainTimerComponent implements OnInit, OnChanges{
 
     }
   }
- 
+
 
 }
