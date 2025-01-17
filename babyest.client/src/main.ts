656d2e7de9548d0550bf296e6b7f953e691a8466
vilalclara <34722004+villaclara@users.noreply.github.com>
2025-01-17 11:52:17 +0200

    /c /s try to fix service-worker to work properly

--- babyest.client/src/main.ts
+++ babyest.client/src/main.ts
@@ -5,13 +5,45 @@ import { bootstrapApplication } from '@angular/platform-browser';
 // import { AppModule } from './app/app.module';
 
 import { provideHttpClient } from '@angular/common/http';
-import { importProvidersFrom } from '@angular/core';
+import { enableProdMode, importProvidersFrom, isDevMode } from '@angular/core';
 import { AppRoutingModule } from './app/app-routing.module';
+import {ServiceWorkerModule} from '@angular/service-worker'
+
 
 // platformBrowserDynamic().bootstrapModule(AppModule, {
 //   ngZoneEventCoalescing: true
 // })
 //   .catch(err => console.error(err));
+
+//ServiceWorkerModule.register('ngsw-worker.js', {
+//  enabled: !isDevMode(),
+//  // Register the ServiceWorker as soon as the application is stable
+//  // or after 30 seconds (whichever comes first).
+//  registrationStrategy: 'registerWhenStable:30000'
+//});
+
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/ngsw-worker.js')
+    .then((registration) => {
+      console.log('Service Worker registered successfully:', registration);
+    })
+    .catch((error) => {
+      console.log('Service Worker registration failed:', error);
+    });
+}
+
+
+ //if ('serviceWorker' in navigator && !isDevMode) {
+ //  navigator.serviceWorker.register('ngsw-worker.js')
+ //    .then((registration) => {
+ //      console.log('Service Worker registered with scope:', registration.scope);
+ //    })
+ //    .catch((error) => {
+ //      console.log('Service Worker registration failed:', error);
+ //    });
+ //}
 
   bootstrapApplication(AppComponent, 
     {
