5f58c741d6d1012bfd9d32ca726db4b729ea49b8
vilalclara <34722004+villaclara@users.noreply.github.com>
2025-01-27 01:30:50 +0200

    /s added sqilte

--- BabyEST.Server/Database/ApplicationDbContext.cs
+++ BabyEST.Server/Database/ApplicationDbContext.cs
@@ -28,12 +28,9 @@ public class ApplicationDbContext : DbContext
 		{
 			b.HasKey(k => k.Id);
 
-			b.HasMany(k => k.Parents)
-			.WithMany(k => k.Kids);
 
-			b.HasMany(k => k.Activities)
-			.WithOne(k => k.Kid)
-			.IsRequired(false);
+
+
 		});
 
 		modelBuilder.Entity<KidActivity>(b =>
