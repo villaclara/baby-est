e4a2b40427d551424c756b2cb89319a72b4c75ae
villaclara <villaclara1231@gmail.com>
2025-02-02 00:08:22 +0200

    /s wip addkid tests

--- tests/ApiTests/IntegrationTests/EndpointTests/KidEndpointTests.cs
+++ tests/ApiTests/IntegrationTests/EndpointTests/KidEndpointTests.cs
@@ -18,6 +18,9 @@ public class KidEndpointTests : IClassFixture<TestWebApplicationFactory<Program>
 		_client = _factory.CreateClient();
 	}
 
+
+	#region GET method
+
 	[Theory]
 	[InlineData("test1@test.com", "password", 2)]
 	[InlineData("test2@test.com", "password", 0)]
@@ -41,8 +44,8 @@ public class KidEndpointTests : IClassFixture<TestWebApplicationFactory<Program>
 	}
 
 	[Theory]
-	[InlineData("test1@test.com", "password", 10)]
-	[InlineData("test2@test.com", "password", 100)]
+	[InlineData("test1@test.com", "password", 4)]   // kid does not belong to parent
+	[InlineData("test2@test.com", "password", 100)] // non-existing kidId
 	public async Task GetKidById_WrongId_Return400(string email, string password, int kidId)
 	{
 		// Arrange 
@@ -83,4 +86,67 @@ public class KidEndpointTests : IClassFixture<TestWebApplicationFactory<Program>
 	}
 
 
+
+	#endregion GET method
+
+	#region POST method
+
+	[Theory]
+	[InlineData("test1@test.com", "password", "newKidParent1", "2025-01-01")]
+	public async Task AddKid_ValidValues_Return201(string email, string password, string kidName, string birthDate)
+	{
+		// Arrange 
+		var kidDto = new KidDto()
+		{
+			Name = kidName,
+			BirthDate = birthDate
+		};
+		var login = await _client.PostAsJsonAsync("/auth/login", new { Email = email, Password = password });
+		login.EnsureSuccessStatusCode();
+
+		// Act + Assert
+		var response = await _client.PostAsJsonAsync<KidDto>(_url, kidDto);
+		Assert.NotNull(response);
+		Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+		Assert.NotNull(response.Headers.Location);
+
+		var kidUrl = response.Headers.Location.ToString();
+		var kidFromApi = await _client.GetAsync(kidUrl);
+
+		Assert.Equal(HttpStatusCode.OK, kidFromApi.StatusCode);
+
+		var content = await kidFromApi.Content.ReadFromJsonAsync<KidDto>();
+
+		Assert.NotNull(content);
+		Assert.Equal(kidName, content.Name);
+		Assert.Equal(birthDate, content.BirthDate);
+
+		// Assert
+	}
+
+
+	public async Task AddKid_WrongParentId_Return400()
+	{
+		// Arrange 
+		var login = await _client.PostAsJsonAsync("/auth/login", new { Email = email, Password = password });
+		login.EnsureSuccessStatusCode();
+
+		var request = new HttpRequestMessage(HttpMethod.Post, _url);
+		request.Headers.
+
+		// Act 
+
+		// Assert
+	}
+
+
+	#endregion POST method
+
+	#region PUT method
+
+	#endregion PUT method
+
+	#region DEL method
+
+	#endregion DEL method
 }
