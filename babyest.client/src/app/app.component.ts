b37a0c8aedb519f6634ee27ea7a2e934715e3dd9
villaclara <villaclara1231@gmail.com>
2025-03-06 01:48:46 +0200

    start dark theme

--- babyest.client/src/app/app.component.ts
+++ babyest.client/src/app/app.component.ts
@@ -1,18 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-interface WeatherForecast {
-  date: string;
-  temperatureC: number;
-  temperatureF: number;
-  summary: string;
-}
-
-class LoginUser {
-  Email: string = 'test1';
-  Password: string = 'string';
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
