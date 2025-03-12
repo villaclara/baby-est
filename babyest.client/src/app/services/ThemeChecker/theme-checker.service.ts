b5ac1c570fa046e0c1deb48c6b6c6958573778c7
villaclara <villaclara1231@gmail.com>
2025-03-13 01:21:26 +0200

    /c check autotheme day hours first.

--- babyest.client/src/app/services/ThemeChecker/theme-checker.service.ts
+++ babyest.client/src/app/services/ThemeChecker/theme-checker.service.ts
@@ -10,34 +10,35 @@ export class ThemeCheckerService {
 
   private subscription!: Subscription;
 
-  start: number = 0;
   constructor(private currentKidService: CurrentKidService) {
-    console.log("number - " + this.start);
   }
 
 
 
   startDoingAction(): void {
     console.log("oninit called");
-    this.subscription = interval(1_000)
+    this.subscription = interval(300_000) // timeout now 5mins
       .subscribe(() => {
-        
-        if(localStorage.getItem('autoTheme') != '1') {
+
+        if (localStorage.getItem('autoTheme') != '1') {
           console.log('1000');
           return;
-          } 
+        }
 
-        if (new Date().getHours() == 21 && this.currentKidService.getTheme() != 'darkTheme') {
+        // Check the day hours first as the value is  7 >= value > 21. 
+        // In the night the value can be > 0 and < 21.
+        if ((new Date().getHours() >= 7 && new Date().getHours() < 21) && this.currentKidService.getTheme() != 'lightTheme') {
+          this.currentKidService.setTheme('lightTheme');
+          console.log('3000');
+
+        }
+
+        else if (this.currentKidService.getTheme() != 'darkTheme') {
           this.currentKidService.setTheme('darkTheme');
           console.log('2000');
 
         }
 
-        else if (new Date().getHours() == 7 && this.currentKidService.getTheme() != 'lightTheme') {
-          this.currentKidService.setTheme('lightTheme');
-          console.log('3000');
-        
-        }
       });
   }
 }
