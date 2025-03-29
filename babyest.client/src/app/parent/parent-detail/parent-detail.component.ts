477df6e0305392fac427b02aac6ca5754267e40f
villaclara <villaclara1231@gmail.com>
2025-03-30 01:04:26 +0200

    v1010 in parent

--- babyest.client/src/app/parent/parent-detail/parent-detail.component.ts
+++ babyest.client/src/app/parent/parent-detail/parent-detail.component.ts
@@ -40,7 +40,7 @@ export class ParentDetailComponent implements OnInit {
   selectedTheme: string = '';
   isAutoThemeChecked: boolean = true;
 
-  version: string = "v1.0.9";
+  version: string = "v1.0.10";
   constructor(private authService: AuthService,
     private router: Router,
     private parentService: ParentService,
