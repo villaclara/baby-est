daef016c3fff049b3614be576357022714d762cc
villaclara <villaclara1231@gmail.com>
2025-04-01 01:58:58 +0300

    /c new loading spinner and overlay

--- babyest.client/src/app/compHelpers/loading-spinner/loading-spinner.component.ts
+++ babyest.client/src/app/compHelpers/loading-spinner/loading-spinner.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-loading-spinner',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './loading-spinner.component.html',
   styleUrl: './loading-spinner.component.css'
 })
 export class LoadingSpinnerComponent {
-
+  @Input() mainColor: string = '#404756';
 }
